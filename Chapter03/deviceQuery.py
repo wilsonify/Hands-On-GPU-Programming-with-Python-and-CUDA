08bed3cd80638ceb787051974f55ab7f733541c5
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-07-16 08:27:39 -0400

    simplify device query

--- Chapter03/deviceQuery.py
+++ Chapter03/deviceQuery.py
@@ -1,5 +1,17 @@
+from pprint import pprint
+
 import pycuda
 import pycuda.driver as drv
+
+capability_to_cores_per_mp_map = {
+    3.0: 128,
+    5.0: 128,
+    5.1: 128,
+    5.2: 128,
+    6.0: 64,
+    6.1: 128,
+    6.2: 128
+}
 
 drv.init()
 
@@ -8,36 +20,31 @@ print('CUDA device query (PyCUDA version) \n')
 print('Detected {} CUDA Capable device(s) \n'.format(drv.Device.count()))
 
 for i in range(drv.Device.count()):
-    
     gpu_device = drv.Device(i)
-    print('Device {}: {}'.format( i, gpu_device.name() )) 
-    compute_capability = float( '%d.%d' % gpu_device.compute_capability() )
-    print('\t Compute Capability: {}'.format(compute_capability))
-    print('\t Total Memory: {} megabytes'.format(gpu_device.total_memory()//(1024**2)))
-    
-    # The following will give us all remaining device attributes as seen 
-    # in the original deviceQuery.
-    # We set up a dictionary as such so that we can easily index
-    # the values using a string descriptor.
-    
-    device_attributes_tuples = iter(gpu_device.get_attributes().items()) 
+    """
+    The following will give us all remaining device attributes as seen
+    in the original deviceQuery.
+    We set up a dictionary as such so that we can easily index
+    the values using a string descriptor.
+    Cores per multiprocessor is not reported by the GPU!
+    We must use a lookup table based on compute capability.
+    See the following:
+    http://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#compute-capabilities
+    """
+
+    compute_capability = float('%d.%d' % gpu_device.compute_capability())
+    cuda_cores_per_mp = capability_to_cores_per_mp_map[compute_capability]
+    total_memory = gpu_device.total_memory() // (1024 ** 2)
+    device_attributes_tuples = iter(gpu_device.get_attributes().items())
     device_attributes = {}
-    
+
     for k, v in device_attributes_tuples:
         device_attributes[str(k)] = v
-    
     num_mp = device_attributes['MULTIPROCESSOR_COUNT']
-    
-    # Cores per multiprocessor is not reported by the GPU!  
-    # We must use a lookup table based on compute capability.
-    # See the following:
-    # http://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#compute-capabilities
-    
-    cuda_cores_per_mp = { 3.0: 128, 5.0 : 128, 5.1 : 128, 5.2 : 128, 6.0 : 64, 6.1 : 128, 6.2 : 128}[compute_capability]
-    
-    print('\t ({}) Multiprocessors, ({}) CUDA Cores / Multiprocessor: {} CUDA Cores'.format(num_mp, cuda_cores_per_mp, num_mp*cuda_cores_per_mp))
-    
-    device_attributes.pop('MULTIPROCESSOR_COUNT')
-    
-    for k in list(device_attributes.keys()):
-        print('\t {}: {}'.format(k, device_attributes[k]))
+    device_attributes["Device #"] = gpu_device.name()
+    device_attributes["Compute Capability"] = compute_capability
+    device_attributes["Total Memory (MB)"] = total_memory
+    device_attributes["Multiprocessors"] = num_mp
+    device_attributes["CUDA Cores / Multiprocessor"] = cuda_cores_per_mp
+    device_attributes["CUDA Cores"] = num_mp * cuda_cores_per_mp
+    pprint(device_attributes)
